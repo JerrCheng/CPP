2168bb7d05c906dd0954bc6f1d24ed588667cf2b
jerry-cheng123 <chr17739159896@163.com>
2023-01-14 22:44:38 +0800

    C++练习

---
+++ BST_oj/BST_oj/test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+int f(int N)
+{
+	if (N == 1)
+		return N;
+	else
+		return N + f(N - 1);
+}
+
+int main()
+{
+	//Debug模式下，到10000就会栈溢出
+	cout << f(1000) << endl;
+	return 0;
+}
